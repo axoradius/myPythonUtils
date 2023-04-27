10b719e5509b6703d5556cbd73c524c89aef88f6
axoradius <97837742+axoradius@users.noreply.github.com>
2023-04-27 12:48:58 +0200

    cfg.json and return list
    working version reading settings from _cfg.json and returning list of files with requested extension.

--- scrp/scrap04_captionAllFiles.py
+++ scrp/scrap04_captionAllFiles.py
@@ -3,26 +3,80 @@ creates a caption for every image in a folder and writes the output to a file
 """
 
 import os
+import json
 
 # set variables
-outputFileName = "catalog_caption.txt"
-sourcePath = "/Users/bvds/Pictures"
+verbose = False
+# identify base path where this file is running from and compose the configFilePath
+curDir = os.getcwd()
+scriptPath = __file__
+scriptName = os.path.basename(__file__).split(".")[0] # basename returns the filename. the split function with separator returns a list with substings. the first is the name without extension
+configFileName = scriptName + "_cfg.json" # TODO: replace with name based upon python file name
+configFilePath = os.path.join(curDir, configFileName)
+allConfigSettings = []
 
-def listAllFiles(srcPath, listExtensions=["jpg","jpeg","png"]):
+
+def getAllConfigValues(cfgPath=configFilePath, verbose=False):
+    if verbose: print(f"start getAllConfigValues on {cfgPath}")
+    with open(cfgPath) as json_file:
+        allCfgSettings = json.load(json_file)
+        if isinstance(allCfgSettings, dict):
+            return allCfgSettings
+        else:
+            print(f"ERROR: {cfgPath} is not a valid JSON configuration file. Check the file.")
+        
+
+def createInitialCfg(cfgFilePath, verbose=False):    
+    if os.path.isfile(cfgFilePath):
+        if verbose: print(f"configuration file {cfgFilePath} exists already")
+    else:
+        writeToFile(['{'],configFilePath)
+        writeToFile(['"sourcePath": "/Users/benny.vandesompele/Pictures",'],configFilePath)
+        writeToFile(['"outputFileName": "catalog_caption.txt",'],configFilePath)
+        writeToFile(['"listExtensions": ["jpg","jpeg","png"]'], configFilePath)
+        writeToFile(['}'],configFilePath)
+        if verbose: print(f"configuration file {cfgFilePath} has been created with default values. Please check the values.")
+
+def listAllFiles(srcPath, listExtensions=["jpg","jpeg","png"], verbose=False):
     result = []
+    i = 0
     if os.path.isdir(srcPath):
         allFiles = os.listdir(srcPath)
         for ext in listExtensions:
+            i += 1
+            if verbose: print(f"iteration {i}: for extension {ext}")
             for f in allFiles:
                 if f.endswith(ext):
-                    print(f"{f} has a valid extension {ext}")
+                    if verbose: print(f"{f} has a valid extension {ext}")
                     result.append(f)
     else:
         print(f"ERROR: {srcPath} is not a valid path")
     return result
 
+
+def writeToFile(messages=[], filePath=""):
+    for msg in messages:
+        myFile = open(filePath,"a")
+        myFile.write(msg)
+
+
 if __name__ == "__main__":
     print(f"::: start :::")
-    res = listAllFiles(sourcePath)
+    createInitialCfg(configFilePath)
+    allConfigSettings = getAllConfigValues(configFilePath)
+    #get parameter values from cfg.json
+    sourcePath = allConfigSettings["sourcePath"]
+    outputFileName = allConfigSettings["outputFileName"]
+    listExtensions = []
+    listExtensions = allConfigSettings["listExtensions"]
+    if len(listExtensions) == 0: 
+        print(f"ERROR: list of extensions in {configFilePath} is empty. Please check")
+        exit
+    if verbose: 
+        print(f"sourcePath is {sourcePath}")
+        print(f"outputFileName is {outputFileName}")
+        print(f"list of extions to filter on is {listExtensions}")
+    res = listAllFiles(sourcePath, listExtensions,verbose)
     nbrFound = len(res)
-    print(f"nbr found is {nbrFound}")
+    print(f"nbr of files with a relevant extension: {nbrFound}")
+    #writeToFile(["hello1","hello2"],"/Users/benny.vandesompele/Pictures/catalog.txt")
